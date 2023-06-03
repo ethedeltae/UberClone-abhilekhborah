30969c6c48538205b9d83dc5e454551fdb155f41
Abhilekh Borah <abhilekhkey@gmail.com>
2023-06-04 01:24:43 +0530

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Abhilekh Borah on 04/06/23.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
